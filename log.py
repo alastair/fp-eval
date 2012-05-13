119dffa4ef649b3d0811329fbd561ff02852717e
Alastair Porter <alastair@porter.net.nz>
2012-05-13 15:41:31 -0400

    Logger

---
+++ log.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s %(levelname) 7s:%(message)s");
+
+fh = logging.FileHandler('fingerprint.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+logger.addHandler(ch)
+
+# If you import the logger, it'll put a header
+#  of the file that is running
+logger.debug("=" * 40)
+logger.debug("Running %s" % sys.argv[0])
+logger.debug("=" * 40)
+
+def debug(msg, *args, **kwargs):
+    logging.debug(msg)
+
+def info(msg, *args, **kwargs):
+    logging.info(msg)
+
+def warning(msg):
+    logging.warning(msg)
