6372d524cc89cdfb7077aad12ba5f5d880dc9ab7
Alastair Porter <alastair@porter.net.nz>
2012-09-06 16:26:46 -0400

    Remove unused code

--- ingest.py
+++ ingest.py
@@ -88,42 +88,6 @@ def main(engine):
     for h in ack_handles:
         thequeue.ack(h)
 
-#XXX: Don't want to continually fingerprint files that had errors
-def xmain(engine):
-    engine_map = fingerprint.fingerprint_index.get(engine)
-    engine_class = engine_map.get("instance") if engine_map else None
-    if not engine_class:
-        raise Exception("%s is not a valid fingerprint engine" % (engine))
-    engine_table = engine_map.get("dbmodel") if engine_map else None
-
-    instance = engine_class()
-
-    log.info("Importing files for engine %s" % (engine))
-    fp_list = []
-    cur = db.session.query(db.FPFile).filter(db.FPFile.negative == False)\
-            .outerjoin(engine_table).filter(engine_table.file_id == None)
-    log.info("got %d things to do stuff with" % cur.count())
-    count = 0
-    total = cur.count()
-    for f in cur:
-        (trackid, fpdata) = instance.fingerprint(f.path)
-        error = "error" in fpdata
-        if not error:
-            e = engine_table(f, trackid)
-            db.session.add(e)
-            fp_list.append(fpdata)
-            count += 1
-        else:
-            log.debug("Error parsing file %s. Error was: %s" % (f, fpdata["error"]))
-
-        # Ingest every 100 songs
-        if len(fp_list) > 99:
-            log.info("Ingesting 100 files at once")
-            log.info("%d/%d done" % (count, total))
-            db.session.commit()
-            instance.ingest_many(fp_list)
-            fp_list = []
-
 def show_fp_engines():
     print "Available fingerprinting engines:"
     print ", ".join(fingerprint.fingerprint_index.keys())
