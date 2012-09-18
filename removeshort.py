e047f6f1363f01f5919d324f33fbac430fbd40b9
Alastair Porter <alastair@porter.net.nz>
2012-09-18 19:02:34 -0400

    Remove results for non-44.1k files

--- removeshort.py
+++ removeshort.py
@@ -4,7 +4,8 @@ import db
 import evaluation
 from chromaprint_support import audioread
 
-def main():
+def short():
+    """Actually delete bad testfiles (and all their results)"""
     countneg = 0
     countpos = 0
     testset_id = 4
@@ -32,5 +33,31 @@ def main():
     print "deleted negative: %s" % countneg
     print "deleted positive: %s" % countpos
 
+def rate():
+    """ Delete results for files that have a non-44.1k samplerate so we can re-do"""
+    testset_id = 4
+    c = 0
+    testfiles = db.session.query(evaluation.Testfile).filter(evaluation.Testfile.testset_id==testset_id)
+    print "Number testfiles: %s" % testfiles.count()
+    for i, tf in enumerate(testfiles):
+        if i % 100 == 0:
+            print i
+        with audioread.audio_open(tf.file.path.encode("utf-8")) as f:
+            rate = f.samplerate
+            if rate != 44100:
+                c += 1
+                print "Unexpected samplerate: %s (%s)" % (tf.file.path.encode("utf-8"), rate)
+                cur = db.session.query(evaluation.Result).filter(evaluation.Result.testfile_id==tf.id)
+                print "%d results to remove" % cur.count()
+                #cur.delete()
+    db.session.commit()
+    testfiles = db.session.query(evaluation.Testfile).filter(evaluation.Testfile.testset_id==testset_id)
+    print "to change", c
+    print "New number testfiles: %s" % testfiles.count()
+
 if __name__ == "__main__":
-    main()
+    import sys
+    if sys.argv[1] == "short":
+        short()
+    elif sys.argv[1] == "rate":
+        rate()
